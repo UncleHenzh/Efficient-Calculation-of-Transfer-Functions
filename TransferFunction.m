ea47dded92ba0714b41165602f75d7cea8066dc5
Zhelun Chen <62721937+UncleHenzh@users.noreply.github.com>
2021-05-04 20:28:33 -0400

    disclaimer added

--- TransferFunction.m
+++ TransferFunction.m
@@ -1,6 +1,21 @@
 function [e,S,n] = TransferFunction(A,B,C,D,dt)
 
-% The algorithm presented here follows Appendix D of
+% You running this script/function means you will not blame the author(s) 
+% if this breaks your stuff. This script/function is provided AS IS without 
+% warranty of any kind. Author(s) disclaim all implied warranties 
+% including, without limitation, any implied warranties of merchantability 
+% or of fitness for a particular purpose. The entire risk arising out of 
+% the use or performance of the sample scripts and documentation remains 
+% with you. In no event shall author(s) be held liable for any damages 
+% whatsoever (including, without limitation, damages for loss of business 
+% profits, business interruption, loss of business information, or other 
+% pecuniary loss) arising out of the use of or inability to use the script
+% or documentation. Neither this script/function, nor any part of it other 
+% than those parts that are explicitly copied from others, may be 
+% republished without author(s) express written permission. Author(s) 
+% retain the right to alter this disclaimer at any time.
+
+% The codes presented here is for the algorithm in Appendix D of
 % Seem, J. E., et al. "Transfer functions for efficient calculation of 
 % multidimensional transient heat transfer." (1989): 5-12.
 
